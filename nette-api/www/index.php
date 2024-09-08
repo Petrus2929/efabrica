20e73deb2fdf9d3a236290da505f9a787c80fa9b
Daniel <danielpetrus2929@gmail.com>
2024-09-08 23:49:57 +0200

    complet appka

--- nette-api/www/index.php
+++ nette-api/www/index.php
@@ -12,11 +12,8 @@ $application = $container->getByType(Nette\Application\Application::class);
 $httpRequest = $container->getByType(Nette\Http\Request::class);
 $httpResponse = $container->getByType(Nette\Http\Response::class);
 
-//registracia udalosti pre spracovanie CORS
 $application->onRequest[] = function (Application $application, Nette\Application\Request $request) use ($httpRequest, $httpResponse): void {
-
-    //nastavenie CORS hlaviciek
-    $httpResponse->setHeader('Access-Control-Allow-Headers', 'Content-Type'); // povolene hlavicky
+    $httpResponse->setHeader('Access-Control-Allow-Headers', 'Content-Type');
 };
 
 $application->run();
